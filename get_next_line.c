3a7165fd13400fe62596138c20d963fcdfc8c5ec
Lucas BENARD <lbenard@student.42.fr>
2018-11-20 19:19:17 +0100

    final version

--- get_next_line.c
+++ get_next_line.c
@@ -3,67 +3,85 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbenard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lbenard <lbenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/10 13:13:57 by lbenard           #+#    #+#             */
-/*   Updated: 2018/11/15 08:00:05 by lbenard          ###   ########.fr       */
+/*   Created: 2018/11/19 09:36:43 by lbenard           #+#    #+#             */
+/*   Updated: 2018/11/20 15:16:54 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#include <stdio.h>
-
-int	fd_cmp(const void *fd, const void *list)
+static void	read_line(t_fd *fd, char (*buffer)[BUFF_SIZE + 1],
+	char **line, char **chr)
 {
-	return (ft_memcmp(fd, list, sizeof(int)));
-}
+	size_t	read_size;
+	char	*tmp;
 
-int	get_line(t_fd *fd, char **line)
-{
-	t_list	*buffer_list;
-	t_list	*last_list;
-	char	last_buffer[BUFF_SIZE + 1];
-	ssize_t	read_size;
-	size_t	chr_len;
-
-	buffer_list = ft_lstnew(fd->buffer, ft_strlen(fd->buffer));
-	last_list = buffer_list;
-	fd->buffer[0] = 0;
-	while (!ft_strchr((char*)last_list->content, '\n'))
+	while (!(*chr = ft_strchr((char*)*line, '\n')))
 	{
-		if ((read_size = read(fd->fd, last_buffer, BUFF_SIZE)) == 0)
-			break ;
-		last_buffer[read_size] = 0;
-		last_list = ft_lstpushback(&last_list,
-			ft_lstnew(last_buffer, read_size + 1));
+		if ((read_size = read(fd->fd, *buffer, BUFF_SIZE)) == 0)
+			return ;
+		(*buffer)[read_size] = 0;
+		tmp = *line;
+		*line = ft_strjoin(*line, *buffer);
+		free(tmp);
+		if (*line == NULL)
+			return ;
 	}
-	fd->buffer[0] = 0;
-	*line = ft_lststrjoin(buffer_list, "");
-	chr_len = ft_strchr(*line, '\n') - *line;
-	(*line)[chr_len] = 0;
-	printf("%s\n", *line);
-	return (1);
 }
 
-int	get_next_line(const int fd, char **line)
+static int	get_line(t_fd *fd, char **line)
+{
+	char	buffer[BUFF_SIZE + 1];
+	char	*chr;
+
+	*line = ft_strdup(fd->buffer);
+	buffer[0] = 0;
+	read_line(fd, &buffer, line, &chr);
+	if (*line == NULL)
+		return (ERROR);
+	if (ft_strlen(buffer) == 0 && ft_strlen(*line) == 0)
+	{
+		free(*line);
+		return (READ_FINISH);
+	}
+	if (chr)
+	{
+		ft_strcpy(fd->buffer, &chr[1]);
+		chr[0] = 0;
+	}
+	else
+		fd->buffer[0] = 0;
+	return (LINE_READ);
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	static t_list	*fd_list = NULL;
 	t_list			*find;
 	t_fd			new_fd;
+	int				retval;
 
-	if (!line)
-		return (-1);
+	if (!line || fd < 0 || read(fd, NULL, 0) < 0)
+		return (ERROR);
 	new_fd.fd = fd;
 	new_fd.buffer[0] = 0;
 	if (!fd_list)
 	{
-		fd_list = ft_lstnew(&new_fd, sizeof(t_fd));
+		if (!(fd_list = ft_lstnew(&new_fd, sizeof(t_fd))))
+			return (ERROR);
 		find = fd_list;
 	}
-	else if (!(find = ft_lstfind(fd_list, &fd, fd_cmp)))
-		ft_lstadd(&fd_list, (find = ft_lstnew(&new_fd, sizeof(t_fd))));
-	return (get_line(find->content, line));
+	else if (!(find = ft_lstcontentfind(fd_list, &fd, sizeof(int))))
+	{
+		if (!(find = ft_lstnew(&new_fd, sizeof(t_fd))))
+			return (ERROR);
+		ft_lstadd(&fd_list, find);
+	}
+	if ((retval = get_line(find->content, line)) == 0)
+		ft_lstremove(&fd_list, find);
+	return (retval);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: lbenard <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 13:16:15 by lbenard           #+#    #+#             */
-/*   Updated: 2018/11/15 04:53:04 by lbenard          ###   ########.fr       */
+/*   Updated: 2018/11/20 15:02:59 by lbenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1024
+# define BUFF_SIZE 2048
 
 # define ERROR -1
 # define READ_FINISH 0
